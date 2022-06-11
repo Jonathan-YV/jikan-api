8187457ca71560502dae438157bfbbf1997c0bb7
Jonathan Vazquez <yair.vazquez001@gmail.com>
2022-06-10 22:33:15 -0500

    .

--- assets/js/buscador.js
+++ assets/js/buscador.js
@@ -1,19 +1,14 @@
 const formulario = document.querySelector("form");
-console.log(window.location.href);
+
 formulario.addEventListener("submit",(e)=>{
 
     e.preventDefault();
     const busqueda = document.querySelector("input").value;
-    console.log(window.location.href.split("/").includes("templates"));
+
     if(window.location.href.split("/").includes("templates")){
         window.location.href = `busqueda.html?value=${busqueda}`;
-        console.log(1);
-        console.log(`busqueda.html?value=${busqueda}`);
     }
     else{
         window.location.href = `templates/busqueda.html?value=${busqueda}`;
-        console.log(2);
-        console.log(`templates/busqueda.html?value=${busqueda}`);
     }
-
 })
